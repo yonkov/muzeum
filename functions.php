1766c5b79ed998f0e418fea8b0bd7a9c45c182b1
yonkov <yonkov.atanas@gmail.com>
2020-11-15 12:07:38 +0200

    change link colors from theme customizer

--- functions.php
+++ functions.php
@@ -167,7 +167,6 @@ function muzeum_scripts() {
 	wp_enqueue_style( 'muzeum-style', get_stylesheet_uri(), array(), MUZEUM_VERSION );
 	wp_style_add_data( 'muzeum-style', 'rtl', 'replace' );
 	// Load Ionicons font
-	//wp_enqueue_script( 'muzeum-ionicons-esm', get_template_directory_uri() . '/static/js/ionicons/ionicons.esm.js', array(), '5.2.3', true );
 	wp_enqueue_script( 'muzeum-ionicons', get_template_directory_uri() . '/static/js/ionicons/ionicons.js', array(), '5.2.3', true );
 
 	wp_enqueue_script( 'muzeum-navigation', get_template_directory_uri() . '/static/js/navigation.js', array(), MUZEUM_VERSION, true );
--- inc/customizer.php
+++ inc/customizer.php
@@ -3,6 +3,8 @@
  * Muzeum Theme Customizer
  *
  * @package Muzeum
+ * @since 1.0.0
+ * 
  */
 
 /**
@@ -15,11 +17,7 @@
 require get_template_directory() . '/inc/sanitization-functions.php';
 
 
-/**
- * Customizer Helper Functions.
- *
- */
-
+/* Customizer Helper Functions. */
 require get_template_directory() . '/inc/customizer-helper.php';
 
 /**
@@ -28,12 +26,11 @@ require get_template_directory() . '/inc/customizer-helper.php';
  * @package muzeum
  * @since 1.0.0
  */
-
 function muzeum_colors_section_customize ($wp_customize) {
 
 	//Primary Menu Background
 	$wp_customize->add_setting( 'main_nav_color' , array(
-		'default'     => "#7c5c0a",
+		'default'     => "#f1ddba",
         'sanitize_callback' => 'sanitize_hex_color'
 	) );
 	$wp_customize->add_control( new WP_Customize_Color_Control( $wp_customize, 'main_nav_color', array(
@@ -44,7 +41,7 @@ function muzeum_colors_section_customize ($wp_customize) {
 
 	//Primary Menu Text Color
     $wp_customize->add_setting('main_nav_text_color', array(
-        'default' => '#fff',
+        'default' => '#000',
         'sanitize_callback' => 'sanitize_hex_color',
     ));
     $wp_customize->add_control(new WP_Customize_Color_Control($wp_customize, 'main_nav_text_color', array(
@@ -57,7 +54,7 @@ function muzeum_colors_section_customize ($wp_customize) {
 
 		//Top Menu Background
 		$wp_customize->add_setting( 'top_nav_color' , array(
-			'default'     => "#7c3c0a",
+			'default'     => "#c45c5b",
 			'sanitize_callback' => 'sanitize_hex_color'
 		) );
 		$wp_customize->add_control( new WP_Customize_Color_Control( $wp_customize, 'top_nav_color', array(
@@ -77,6 +74,39 @@ function muzeum_colors_section_customize ($wp_customize) {
 		)));
 	
 	endif;
+
+    // Headings color
+	$wp_customize->add_setting('headings_textcolor', array(
+		'default' => "#333333",
+		'sanitize_callback' => 'sanitize_hex_color'
+	));
+
+	$wp_customize->add_control(new WP_Customize_Color_Control($wp_customize, 'headings_textcolor', array(
+		'label' => esc_html__('Headings Text Color', 'estera'),
+		'section' => 'colors',
+	)));
+
+	// Links color
+	$wp_customize->add_setting('links_textcolor', array(
+		'default' => "#253e80",
+		'sanitize_callback' => 'sanitize_hex_color'
+	));
+
+	$wp_customize->add_control(new WP_Customize_Color_Control($wp_customize, 'links_textcolor', array(
+		'label' => esc_html__('Links Text Color', 'estera'),
+		'section' => 'colors',
+	)));
+
+	// Sidebar Links Text color
+	$wp_customize->add_setting('sidebar_link_textcolor', array(
+		'default' => "#666",
+		'sanitize_callback' => 'sanitize_hex_color',
+	));
+
+	$wp_customize->add_control(new WP_Customize_Color_Control($wp_customize, 'sidebar_link_textcolor', array(
+		'label' => esc_html__('Sidebar Links Color', 'estera'),
+		'section' => 'colors',
+	)));
 
 	// Enable custom logo
 	if (function_exists('the_custom_logo')) :
@@ -101,33 +131,37 @@ function muzeum_colors_section_customize ($wp_customize) {
 add_action('customize_register', 'muzeum_colors_section_customize');
 
 /**
- * Output the Customizer CSS to wp_head
+ * Output the Customizer Color Section CSS to wp_head
  */
 function muzeum_customizer_css() {
 
-	$primary_menu_color = get_theme_mod( 'main_nav_color', '#7c5c0a');
-	$top_menu_color = get_theme_mod( 'top_nav_color', '#7c3c0a' ); 
+	$primary_menu_color = get_theme_mod( 'main_nav_color', '#f1ddba');
+	$top_menu_color = get_theme_mod( 'top_nav_color', '#c45c5b' ); 
 	
-	$primary_menu_text_color = get_theme_mod('main_nav_text_color', '#fff'); 
+	$primary_menu_text_color = get_theme_mod('main_nav_text_color', '#000'); 
 	$top_menu_text_color = get_theme_mod('top_nav_text_color', '#fff'); 
+
+	$links_text_color = get_theme_mod('links_textcolor', '#253e80');
+	$headings_text_color = get_theme_mod('headings_textcolor', '#333');
+	$sidebar_text_color = get_theme_mod('sidebar_link_textcolor', '#666');
 	
 	?>
 
 	<style>
 		.main-nav {
-			background-color: <?php echo sanitize_hex_color( $primary_menu_color ); // WPCS: XSS ok. ?>;
+			background-color: <?php echo esc_attr( $primary_menu_color ); // WPCS: XSS ok. ?>;
 		}
 
 		.main-nav li:hover, .main-nav li.focus {
-			background-color: <?php echo muzeum_brightness( $primary_menu_color, -20 ); // WPCS: XSS ok. ?>;
+			background-color: <?php echo esc_attr(muzeum_brightness( $primary_menu_color, -20 )); // WPCS: XSS ok. ?>;
 		}
 
 		.main-nav .menu-toggle {
-			background-color: <?php echo sanitize_hex_color( $primary_menu_color ); // WPCS: XSS ok. ?>;
+			background-color: <?php echo esc_attr( $primary_menu_color ); // WPCS: XSS ok. ?>;
 		}
 
 		.main-nav.toggled .menu-toggle {
-			background-color: <?php echo muzeum_brightness( $primary_menu_color, -50 ); // WPCS: XSS ok. ?>;
+			background-color: <?php echo esc_attr(muzeum_brightness( $primary_menu_color, -50 )); // WPCS: XSS ok. ?>;
 		}
 
 		.main-nav a {
@@ -137,18 +171,18 @@ function muzeum_customizer_css() {
 		@media (min-width:40em){
 
 			.main-nav ul ul li {
-				background-color: <?php echo muzeum_brightness( $primary_menu_color, -35 ); // WPCS: XSS ok. ?>;
+				background-color: <?php echo esc_attr(muzeum_brightness( $primary_menu_color, -35 )); // WPCS: XSS ok. ?>;
 			}
 			.main-nav li li:hover,
 			.main-nav li li.focus {
-				background-color: <?php echo muzeum_brightness( $primary_menu_color, -50 ); // WPCS: XSS ok. ?>;
+				background-color: <?php echo esc_attr(muzeum_brightness( $primary_menu_color, -50 )); // WPCS: XSS ok. ?>;
 			}
 		}
 
 	<?php if ( has_nav_menu( 'menu-1' ) ) : ?>
 
 		.top-nav {
-			background-color: <?php echo sanitize_hex_color( $top_menu_color ); // WPCS: XSS ok. ?>;
+			background-color: <?php echo esc_attr( $top_menu_color ); // WPCS: XSS ok. ?>;
 		}
 
 		.top-nav li:hover, .top-nav li.focus {
@@ -156,7 +190,7 @@ function muzeum_customizer_css() {
 		}
 
 		.top-nav .menu-toggle {
-			background-color: <?php echo sanitize_hex_color( $top_menu_color ); // WPCS: XSS ok. ?>;
+			background-color: <?php echo esc_attr( $top_menu_color ); // WPCS: XSS ok. ?>;
 		}
 
 		.top-nav.toggled .menu-toggle {
@@ -177,6 +211,18 @@ function muzeum_customizer_css() {
 				background-color: <?php echo muzeum_brightness( $top_menu_color, -50 ); // WPCS: XSS ok. ?>;
 			}
 
+		}
+
+		a {
+			color: <?php echo esc_attr($links_text_color); ?>;
+		}
+
+		.entry-title a {
+			color: <?php echo esc_attr($headings_text_color); ?>;
+		}
+
+		.widget ul a {
+			color: <?php echo esc_attr($sidebar_text_color); ?>;
 		}
 
 	<?php endif; ?>
